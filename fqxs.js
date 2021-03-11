5f28848a18e6be08afb0d674ce5e46f3404b3a6f
QUAN <79043893+1053193027@users.noreply.github.com>
2021-03-11 20:23:54 +0800

    Update fqxs.js

--- fqxs.js
+++ fqxs.js
@@ -1,10 +1,7 @@
 let url = $request.url;
 let body = JSON.parse($response.body);
 
-if (url.indexOf(user) != -1) {
-	body.data.isvip = 1;
-}
-
-	body = JSON.stringify(body);
+body.data.isvip = 1;
+body = JSON.stringify(body);
 
 $done({body});
